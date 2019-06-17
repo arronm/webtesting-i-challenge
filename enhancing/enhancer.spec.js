91e8e24030ab02bdbb6a3b548707a1c2184018de
Arron M <marshall.arron@gmail.com>
2019-06-17 15:57:26 -0700

    Set up initial project on local with test structure

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,29 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+let item = {
+  name: 'Helmet',
+  durability: 23,
+  enhancement: 0,
+};
+
+describe('enhancer', () => {
+  describe('repairs', () => {
+    it.todo('an items durability to 100');
+    it.todo('cannot have a durability of < 0 or > 100');
+  });
+
+  describe('succeeds', () => {
+    it.todo('cannot have an enhancement below or above 20');
+    it.todo('will not increase enhancement over 20');
+    it.todo('will increment the enhancement level');
+  });
+
+  describe('fails', () => {
+    it.todo('will reduce the durability based on initial enhancement level');
+    it.todo('will reduce the enhancement level by 1 if it is greater than 16 initially');
+  })
+
+  describe('gets', () => {
+    it.todo('will get the name of the item based on the enhancement level');
+  })
+});
